02b524e1bc61297abfef53123179c51b2427d10e
lucca-t <luccata04@gmail.com>
2025-10-29 00:13:24 -0600

    more images

--- practica3/script.js
+++ practica3/script.js
@@ -1,53 +1,57 @@
-// Función para reproducir el sonido de fart
-function playFart() {
-  const audio = new Audio("../assets/fart.mp3");
-  audio.volume = 0.1;
-  audio.play();
-}
-function changeDefault() {
-  let img = document.getElementById("myImage");
-  img.src = "../assets/default.png";
 
-  document.body.style.backgroundImage = "url('../assets/title.jpg')";
+function playMusic() {
+    
+    const music = document.getElementById("music");
+    music.volume = 0.1;
+
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+    audio.volume = 0.1;
+    audio.play();
+}
+
+const musicBtn = document.getElementById("musicButton");
+
+musicBtn.addEventListener("click", function (e) {
+    playMusic();
+});
+
+
+function changeDefault() {
+    let img = document.getElementById("myImage");
+    img.src = "../assets/default.png";
+
+    document.body.style.backgroundImage = "url('../assets/title.jpg')";
 }
 function changeChaos() {
-  let img = document.getElementById("myImage");
-  img.src = "../assets/kratoschaos.png";
-  img.style.width = "500px";
+    let img = document.getElementById("myImage");
+    img.src = "../assets/kratoschaos.png";
+    img.style.width = "500px";
 
-  document.body.style.backgroundImage = "url('../assets/background1.jpg')";
+    document.body.style.backgroundImage = "url('../assets/background1.jpg')";
 }
 
 function changeAthena() {
-  let img = document.getElementById("myImage");
-  img.src = "../assets/kratosathena.png";
-  img.style.width = "500px";
-    
-  document.body.style.backgroundImage = "url('../assets/chaosbackground.jpg')";
-}
-function changeAttack() {
-  let img = document.getElementById("myImage");
-  img.src = "../assets/attack.png";
-  const music = document.getElementById("music");
-  music.volume = 0.1;
+    let img = document.getElementById("myImage");
+    img.src = "../assets/kratosathena.png";
+    img.style.width = "500px";
 
-  if (music.paused) {
-    music.play();
-  } else {
-    music.pause();
-  }
+    document.body.style.backgroundImage = "url('../assets/chaosbackground.jpg')";
+}
+function changeOld() {
+    let img = document.getElementById("myImage");
+    img.src = "../assets/old.png";
+    document.body.style.backgroundImage = "url('../assets/norse.avif')";
 }
 
 function changeFortnite() {
-  let img = document.getElementById("myImage");
-  img.src = "../assets/kratosgriddy.gif";
-  img.style.width = "500px";
+    let img = document.getElementById("myImage");
+    img.src = "../assets/kratosgriddy.gif";
+    img.style.width = "500px";
 
-  document.body.style.backgroundImage = "url('../assets/fortnitebackground.png')";
+    document.body.style.backgroundImage = "url('../assets/fortnitebackground.png')";
 }
 
-const fartBtn = document.getElementById("fartButton");
-
-fartBtn.addEventListener("click", function (e) {
-  playFart();
-});
